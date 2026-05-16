99661c032710a1f5df48b909eb179012eb904fb9
Sofiaperez77 <sofiaperezpadilla2@gmail.com>
2026-05-16 11:36:26 -0600

    comentar la clase para mejorar entendimiento

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hola mundo")
+#Inicializar variables
+nombre="Sofia" 
+#Solicitar al usuario su edad 
+edad:int=18
+
+#calcular año de nacimiento
+anno_de_nacimiento:int=2026-edad
+
+#Imprimir nombre,edad y año de nacimiento
+print(anno_de_nacimiento)
+mayor_de_edad=edad>=18
+print(mayor_de_edad)
+soy_yo=npmbre=="sofia"and edad==18)
+#calcular mayoria de edad
+
+
+#Imprimir variables en pantalla
+print(soy_yo)
+no_soy_yo= not (nombre=="sofia"and edad ==18)
+soy_yo=not(nombre=="sofia"and edad==18)
+talvez__soy_yo=not(nombre=="sofia" and edad )
+
+print(soy_yo)
+print(no_soy_yo)
+print(quizas_soy_yo)
+soy_yo=(nombre="sofia"and edad ==18)
