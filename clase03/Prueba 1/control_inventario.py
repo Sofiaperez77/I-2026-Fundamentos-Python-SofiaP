c836a38db8caf7933573ff2450eecb1469cc9411
Sofiaperez77 <sofiaperezpadilla2@gmail.com>
2026-05-30 11:53:21 -0600

    evaluacion 1

---
+++ clase03/Prueba 1/control_inventario.py
@@ -0,0 +1,6 @@
+producto=input("Ingrse el nombre de producto")
+Cantidad=int(input("Ingrese la cantidad de producto"))
+precio=float(input("Ingrese el precio del producto:"))
+total= precio*Cantidad
+print("El total a pagar es: ", total )
+while =
