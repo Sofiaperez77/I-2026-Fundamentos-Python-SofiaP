00fba2e7768835d18c6d01f3c7cfbb2e7a78ddbb
Sofiaperez77 <sofiaperezpadilla2@gmail.com>
2026-06-13 11:18:09 -0600

    clase06

--- Clase06/Actividad04.py
+++ Clase06/Actividad04.py
@@ -1,11 +1,22 @@
+contador = 0
+while true
 print("biemvenidos al registro de estudiantes")
-While True
 Nombre= input("ingrese su nombre: ")
 Carnet = input("Ingrse su carnet: ")
 Nota = float(input("ingrese su nota final: "))
+
+#Contador de estudiantes registrados
+Contador += 1
+print("Registro de estudiantes guardado exitosamente")
+print("Total de estudiantes registrados:" contador)
+
 
 archivo = open("Clase06\Estudiantes.txt" , "a")
 archivo.write(f"nombre:   {Nombre}\n")
 archivo.write(f"carnet   {Carnet}\n")
 archivo.write(f"nota final:   {Nota}\n")
 archivo.close()
+
+continuar = input("¿Desea registrar otro estudiante? (si/no):")
+if continuar.lower()=="no":
+ break
